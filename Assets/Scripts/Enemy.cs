fd780cf912dc849e2b8ed81a2a5aef2100856b19
YacineL <yacine9258@gmail.com>
2019-08-25 20:26:05 +0100

    [EVO] : adds particles collisions
    makes enemies disappear when hit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        AddNonTriggerBoxCollide();
+    }
+
+    private void AddNonTriggerBoxCollide()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+    }
+}
